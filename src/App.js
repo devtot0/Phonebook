bd7af939111ca38a0f35715de546ccf64177d4cf
devtot0 <33835148+devtot0@users.noreply.github.com>
2021-08-18 20:29:52 +0200

    added post request for a person object

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
@@ -5,10 +6,10 @@ import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "000-000-000","id": 1},
-    { name: "Ada Lovelace", number: "39-44-5323523","id": 2},
-    { name: "Dan Abramov", number: "12-43-234345","id": 3},
-    { name: "Mary Poppendieck", number: "39-23-6423122","id": 4 },
+    { name: "Arto Hellas", number: "000-000-000", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -31,9 +32,14 @@ const App = () => {
         id: persons.length + 1,
       };
 
-      setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          console.log(response);
+          setPersons(persons.concat(personObject));
+          setNewName("");
+          setNewNumber("");
+        });
     }
   };
 
